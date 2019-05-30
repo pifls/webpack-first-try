a80fc2b3995155ca4f1e1ae32c3ea314d95256d3
pifls <marek.pikosz@gmail.com>
2019-05-30 12:01:40 +0200

    webpack - working with images

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,13 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
                 test: /\.html$/,
                     use: [
                         {
@@ -11,6 +18,12 @@ module.exports = {
                             options: { minimize: true }
                         }
                     ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     },
